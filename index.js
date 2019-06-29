685567d7fe458227219a2a94ce3324477c3c1220
Km81 <km81.ryu@icloud.com>
2019-06-29 16:23:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,11 @@ SamsungAirco1.prototype = {
             .on('get', this.getHeatingUpOrDwTemperature.bind(this))
             .on('set', this.setHeatingUpOrDwTemperature.bind(this));
 
-
+        this.aircoSamsung.getCharacteristic(Characteristic.SwingMode)
+            .on('get', this.getSwingMode.bind(this))
+            .on('set', this.setSwingMode.bind(this));
+        
+        
         var informationService = new Service.AccessoryInformation();
 
         return [informationService, this.aircoSamsung];
@@ -173,7 +177,80 @@ SamsungAirco1.prototype = {
 
     },
 
+    getSwingMode: function(callback) {
+        var body;
+        
+        str = 'curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer ' + this.token + '" --cert ' + this.patchCert + ' --insecure -X GET https://' + this.ip + ':8888/devices|jq \'.Devices[0].Wind.direction\'';
 
+        this.log(str);
+
+        this.execRequest(str, body, function(error, stdout, stderr) {
+            if (error) {
+                this.log('Power function failed', stderr);
+                callback(error);
+            } else {
+                this.log('Power function OK');
+                this.log(stdout);
+                this.response = stdout;
+                this.response = this.response.substr(1, this.response.length - 3);
+                this.log(this.response);
+                //callback();
+
+            }
+            if (this.response == "Fix") {
+                callback(null, Characteristic.SwingMode.SWING_DISABLED);
+                this.log(this.response + "방향 고정됨");
+            } else if (this.response == "Up_And_Low") {
+                this.log("회전 설정됨");
+                callback(null, Characteristic.SwingMode.SWING_ENABLED);
+            } else {
+                this.log(this.response + "회전 설정 오류");
+            }
+        }.bind(this));
+
+    },
+    
+    
+    setSwingMode: function(state, callback) {
+        var body;
+        var token, ip, patchCert;
+        token = this.token;
+        ip = this.ip;
+        patchCert = this.patchCert;
+
+        this.log("SwingMode");
+        this.log(state);
+        this.log(ip);
+        var activeFuncion = function(state) {
+            if (state == Characteristic.SwingMode.SWING_ENABLED) {
+               str = 'curl -k -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '" --cert ' + patchCert + ' --insecure -X PUT -d \'{"direction": "Up_And_Low"}\' https://' + ip + ':8888/devices/1/wind';
+                console.log("방향고정모드");
+                 } 
+             else if (state == Characteristic.SwingMode.SWING_DISABLED) {
+                str = 'curl -k -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '" --cert ' + patchCert + ' --insecure -X PUT -d \'{"direction": "Fix"}\' https://' + ip + ':8888/devices/1/wind';
+                console.log("회전모드해제");
+                 } 
+            else {
+                console.log("회전모드 오류");
+            }
+        }
+        activeFuncion(state);
+        this.log(str);
+
+        this.execRequest(str, body, function(error, stdout, stderr) {
+            if (error) {
+                this.log('Power function failed', stderr);
+            } else {
+                this.log('Power function OK');
+                //callback();
+                this.log(stdout);
+            }
+        }.bind(this));
+        callback();
+    },
+    
+    
+    
     getActive: function(callback) {
         var body;
         var OFForON;
